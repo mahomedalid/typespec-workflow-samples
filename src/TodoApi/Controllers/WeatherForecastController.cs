5ed3d7720166762e7bbfa0d110de10d3c70771ab
mahomedalid <mictlan@gmail.com>
2023-03-19 05:57:56 +0000

    Fixing code

--- src/TodoApi/Controllers/WeatherForecastController.cs
+++ src/TodoApi/Controllers/WeatherForecastController.cs
@@ -23,7 +23,7 @@ public class WeatherForecastController : ControllerBase
     {
         return new WeatherForecast
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(1)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         };
